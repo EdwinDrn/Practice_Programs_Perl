41b5766252a8ff86f337405b7e8206d66ac849e8
Edwin Duran <31642430+EdwinDrn@users.noreply.github.com>
2020-07-08 12:31:04 -0400

    Add files via upload

---
+++ if_loop.pl
@@ -0,0 +1,25 @@
+#if loop program
+#Created on May 7, 2020 by Edwin Duran
+
+#Take a given set variable '11'
+my $var1 = 11;
+#$var2 = 11;
+
+#If the variable meets the if condition, the appropriate response is printed.
+if ($var1 == 2){
+  #echo "Edwin"
+  print "This is a basic if loop.\n";
+}
+else{
+  print "Wrong password.\n";
+}
+
+#Below is test code that I commented out.
+
+#print "What is your age? ";
+#my $age = <STDIN>;
+#if ($age >= 18) {
+#    print "In most countries you can vote.\n";
+#} else {
+#    print "You are too young to vote\n";
+#}
