5670b84fc27e9711fb5362f0f2dfa7b76a6fc30c
Edwin Duran <31642430+EdwinDrn@users.noreply.github.com>
2020-07-08 12:20:42 -0400

    Add files via upload

---
+++ for_loop.pl
@@ -0,0 +1,10 @@
+#for loop test program
+#Created on May 12, 2020 by Edwin Duran.
+
+#take an integer between
+@a = (1..10);
+
+#For every variable (a), print each number on a new line.
+for (@a){
+  print ("$_","\n");
+}
