6a9e879f4458ec53f614b9f0ec3914bb43a009a6
Edwin Duran <31642430+EdwinDrn@users.noreply.github.com>
2020-07-08 12:47:54 -0400

    Add files via upload

---
+++ perl_practice.pl
@@ -0,0 +1,27 @@
+#Perl syntax practice
+#Program created on March 12, 2020, by Edwin Duran.
+#Program outputs variables and multiplies several together. 
+
+#New line variable
+$new_line ="\n";
+$a = 1;
+$b = 10;
+$c = $a + $b;
+
+
+$x = "My name is Edwin, and my favorite number is ";
+
+$y = "11 \n";
+
+
+print $x;
+print $y;
+print $c;
+print $new_line;
+
+$e = 50;
+$e = $e * 1.05;
+print $e
+print $new_line
+print $e
+#print $new_line;
